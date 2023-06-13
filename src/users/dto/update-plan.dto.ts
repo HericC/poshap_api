e706b5eb8f1d3f0be78f1ce43f88659eafeed0af
Heric Macedo Oliveira Silva <heric_dev@hotmail.com>
2023-06-13 08:56:06 -0300

    feat: type of contracting the plan and wallet debit of the plan value

--- src/users/dto/update-plan.dto.ts
+++ src/users/dto/update-plan.dto.ts
@@ -1,9 +1,35 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
+export enum TypePlan {
+  monthly = 'monthly',
+  quarterly = 'quarterly',
+  semester = 'semester',
+}
+
+export enum TypePayment {
+  wallet = 'wallet',
+  bankSlip = 'bankSlip',
+  pix = 'pix',
+}
+
 export class UpdatePlanDto {
   @ApiProperty()
   @IsString({ message: 'A chave do plano deve ser um texto' })
   @IsNotEmpty({ message: 'É necessário informar a chave do plano' })
   key: string;
+
+  @ApiProperty({
+    description: 'Tipo de pagamento',
+    enum: TypePlan,
+  })
+  @IsNotEmpty()
+  typePlan: TypePlan;
+
+  @ApiProperty({
+    description: 'Tipo de pagamento',
+    enum: TypePayment,
+  })
+  @IsNotEmpty()
+  typePayment: TypePayment;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Plan, User } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { UpdatePlanDto } from './dto/update-plan.dto';
+import {
+  TypePayment as TypePaymentPlan,
+  TypePlan,
+  UpdatePlanDto,
+} from './dto/update-plan.dto';
 import {
   TypePayment as TypePaymentWallet,
   UpdateWalletDto,
@@ -63,13 +68,49 @@ export class UsersService {
     });
   }
 
-  async updatePlan({ key }: UpdatePlanDto, userId: string) {
+  private async updatePlanWallet(plan: Plan, typePlan: TypePlan, user: User) {
+    const price = plan[`${typePlan}Price`];
+    if (!price) throw new DatabaseError('Tipo do plano não encontrado.');
+
+    const wallet = user.wallet - price;
+    if (wallet < 0) throw new DatabaseError('Não possui saldo suficiente.');
+
+    return this.usersRepository.update(user.id, {
+      planKey: plan.key,
+      planDate: new Date(),
+      wallet,
+    });
+  }
+
+  async updatePlan(updatePlanDto: UpdatePlanDto, userId: string) {
+    const { key, typePlan, typePayment } = updatePlanDto;
+
+    const type = TypePaymentPlan[typePayment];
+    if (!type) throw new NotFoundError('Tipo de pagamento não disponivel.');
+
     const plan = await this.plansRepository.findOne(key);
     if (!plan) throw new DatabaseError('Plano não encontrado.');
 
-    // future implementation
+    const user = await this.findOne(userId);
+    if (plan.key === user.planKey)
+      throw new DatabaseError('Já possui este plano.');
 
-    return this.usersRepository.update(userId, { planKey: key });
+    if (
+      plan.key === 'basic' ||
+      (plan.key === 'silver' && user.planKey === 'gold')
+    )
+      throw new DatabaseError('Plano inferior ao plano atual.');
+
+    if (type === 'wallet')
+      return this.updatePlanWallet(plan, typePlan, user as any);
+
+    if (type === 'bankSlip') {
+      /* future payment implementation */
+    }
+
+    if (type === 'pix') {
+      /* future payment implementation */
+    }
   }
 
   async deposit(updateWalletDto: UpdateWalletDto, userId: string) {
